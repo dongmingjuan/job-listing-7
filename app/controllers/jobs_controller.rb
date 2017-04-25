d48e581f9ba7224a2f6be2b8de62a0e5a5f57890
dongmingjuan <d387918156@gmail.com>
2017-04-25 11:02:57 +0800

    join & quit group action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy, :join, :quit]
   layout "front"
   def index
     @jobs = case params[:order]
@@ -47,6 +47,27 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
     @job.destroy
     redirect_to jobs_path
+  end
+  def join
+    @job = Job.find(params[:id])
+
+    if !current_user.is_member_of?(@job)
+      current_user.join!(@job)
+      flash[:notice] = "加入本讨论版成功!"
+    end
+
+    redirect_to job_path(@job)
+  end
+
+  def quit
+    @job = Job.find(params[:id])
+
+    if current_user.is_member_of?(@job)
+      current_user.quit!(@job)
+      flash[:alert] ="已退出本讨论版"
+    end
+
+    redirect_to job_path(@job)
   end
   private
   def job_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :jobs do
+    member do
+      post :join
+      post :quit
+    end
     resources :resumes
   end
 
