73b163806052fc80d07e0ff09034eee40d4175f9
dongmingjuan <d387918156@gmail.com>
2017-04-25 17:19:31 +0800

    implement the edit & delete button

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -11,12 +11,37 @@ class ResumesController < ApplicationController
     @resume.job = @job
     @resume.user = current_user
     if @resume.save
-      flash[:notice] = "成功提交履历"
+      flash[:notice] = "成功提交post"
       redirect_to job_path(@job)
     else
       render :new
     end
   end
+
+  def edit
+    @job = Job.find(params[:job_id])
+    @resume = Resume.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:job_id])
+    @resume = Resume.find(params[:id])
+
+    if @resume.update(resume_params)
+      flash[:notice] = "修改post成功！"
+      redirect_to post_resumes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:job_id])
+
+    @resume.destroy
+    flash[:alert] = "Post 已删除"
+    redirect_to post_resumes_path
+ end
 
 
 
